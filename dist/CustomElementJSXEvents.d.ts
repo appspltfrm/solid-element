a705253dafe4103236862696d6da9853817a41b0
Mark Chris Levy <levy@codeandmore.pl>
2023-04-10 18:59:45 +0200

    Initial commit

--- dist/CustomElementJSXEvents.d.ts
+++ dist/CustomElementJSXEvents.d.ts
@@ -5,7 +5,13 @@ export type CustomElementJSXEvents<Element extends CustomElement, Type extends {
 }> = {
     [P in keyof Type as `on${Capitalize<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
 } & {
+    [P in keyof Type as `on${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
+} & {
+    [P in keyof Type as `on:${string & P}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
+} & {
     [P in keyof Type as `on:${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
+} & {
+    [P in keyof Type as `oncapture:${string & P}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
 } & {
     [P in keyof Type as `oncapture:${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
 };
--- src/lib/CustomElementJSXEvents.ts
+++ src/lib/CustomElementJSXEvents.ts
@@ -4,7 +4,13 @@ import {CustomElement} from "./customElement";
 export type CustomElementJSXEvents<Element extends CustomElement, Type extends {[P in keyof Type]: Event}> = {
     [P in keyof Type as `on${Capitalize<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>
 } & {
+    [P in keyof Type as `on${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>
+} & {
+    [P in keyof Type as `on:${string & P}`]?: JSX.EventHandlerUnion<Element, Type[P]>
+} & {
     [P in keyof Type as `on:${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>
+} & {
+    [P in keyof Type as `oncapture:${string & P}`]?: JSX.EventHandlerUnion<Element, Type[P]>
 } & {
     [P in keyof Type as `oncapture:${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>
 }
