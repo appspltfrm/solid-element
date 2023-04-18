6ccd1033ef8a8052446230ef0ea25dea2874c8f0
Mark Chris Levy <levy@codeandmore.pl>
2023-04-18 22:30:34 +0200

    Initial commit

--- dist/defineLazyCustomElement.js
+++ dist/defineLazyCustomElement.js
@@ -1,29 +1,33 @@
-import { customElementBirthmark as l } from "./customElementBirthmark.js";
-import { buildFinalClass as r } from "./internals/buildFinalClass.js";
-const n = {}, m = new MutationObserver(async (t) => {
-  let s = [];
-  for (const f of t)
-    if (f.addedNodes) {
-      for (const o of f.addedNodes)
-        if (o instanceof Element && n[o.tagName]) {
-          if (!customElements.get(o.tagName)) {
-            let e = await n[o.tagName]();
-            typeof e == "object" && (e = e.default), typeof e == "function" && e.defineCustomElement ? e.defineCustomElement() : (e[l] && (e = r(e)), customElements.define(o.tagName.toLowerCase(), e)), s.push(o.tagName);
-          }
-          s.includes(o.tagName) && customElements.upgrade(o);
+import { customElementBirthmark as d } from "./customElementBirthmark.js";
+import { buildFinalClass as a } from "./internals/buildFinalClass.js";
+const i = {}, f = {}, r = new MutationObserver(async (t) => {
+  let o = [];
+  for (const l of t)
+    if (l.addedNodes) {
+      for (const n of l.addedNodes)
+        if (n instanceof Element && i[n.tagName]) {
+          const s = n.tagName;
+          !o.includes(s) && !customElements.get(s) && (s in f || (f[s] = new Promise(async (c) => {
+            try {
+              let e = await i[n.tagName]();
+              typeof e == "object" && (e = e.default), typeof e == "function" && e.defineCustomElement ? e.defineCustomElement() : (e[d] && (e = a(e)), customElements.define(n.tagName.toLowerCase(), e)), c();
+            } catch (e) {
+              console.error(e), delete f[s];
+            }
+          })), await f[s], o.push(n.tagName)), o.includes(n.tagName) && customElements.upgrade(n);
         }
     }
-  for (const f of s)
-    delete n[f];
-  Object.keys(n).length === 0 && m.disconnect();
+  for (const l of o)
+    delete i[l], delete f[l];
+  Object.keys(i).length === 0 && r.disconnect();
 });
-let i = !1;
-function u(t, s) {
-  if (t = t.toUpperCase(), customElements.get(t) || n[t])
+let m = !1;
+function p(t, o) {
+  if (t = t.toUpperCase(), customElements.get(t) || i[t])
     throw new Error(`Custom element ${t} already defined`);
-  n[t] = s, i || (i = !0, m.observe(document, { subtree: !0, childList: !0 }));
+  i[t] = o, m || (m = !0, r.observe(document, { subtree: !0, childList: !0 }));
 }
 export {
-  u as defineLazyCustomElement
+  p as defineLazyCustomElement
 };
 //# sourceMappingURL=defineLazyCustomElement.js.map
--- src/lib/defineLazyCustomElement.ts
+++ src/lib/defineLazyCustomElement.ts
@@ -7,6 +7,7 @@ import {InternalClass} from "./internals/InternalClass";
 type LazyType = Promise<CustomElementConstructor | {default: CustomElementConstructor} | CustomElementComponent<any, CustomElement> | {default: CustomElementComponent<any, CustomElement>}>;
 
 const elements: {[tagName: string]: () => LazyType} = {};
+const loading: {[tagName: string]: Promise<void>} = {};
 
 const observer = new MutationObserver(async (mutations) => {
 
@@ -16,26 +17,41 @@ const observer = new MutationObserver(async (mutations) => {
     for (const m of mutations) {
         if (m.addedNodes) {
             for (const n of m.addedNodes) {
+
                 if (n instanceof Element && elements[n.tagName]) {
+                    const tagName = n.tagName;
 
-                    if (!customElements.get(n.tagName)) {
+                    if (!definedElements.includes(tagName) && !customElements.get(tagName)) {
 
-                        let elementClass = await elements[n.tagName]();
-                        if (typeof elementClass === "object") {
-                            elementClass = elementClass.default;
+                        if (!(tagName in loading)) {
+                            loading[tagName] = new Promise(async (resolve) => {
+                                try {
+                                    let elementClass = await elements[n.tagName]();
+                                    if (typeof elementClass === "object") {
+                                        elementClass = elementClass.default;
+                                    }
+
+                                    if (typeof elementClass === "function" && (elementClass as unknown as CustomElementComponent<any, CustomElement>).defineCustomElement) {
+                                        (elementClass as unknown as CustomElementComponent<any, CustomElement>).defineCustomElement();
+
+                                    } else {
+
+                                        if ((elementClass as unknown as InternalClass)[customElementBirthmark]) {
+                                            elementClass = buildFinalClass(elementClass as any);
+                                        }
+
+                                        customElements.define(n.tagName.toLowerCase(), elementClass as any);
+                                    }
+
+                                    resolve();
+                                } catch (e) {
+                                    console.error(e);
+                                    delete loading[tagName];
+                                }
+                            })
                         }
 
-                        if (typeof elementClass === "function" && (elementClass as unknown as CustomElementComponent<any, CustomElement>).defineCustomElement) {
-                            (elementClass as unknown as CustomElementComponent<any, CustomElement>).defineCustomElement();
-
-                        } else {
-
-                            if ((elementClass as unknown as InternalClass)[customElementBirthmark]) {
-                                elementClass = buildFinalClass(elementClass as any);
-                            }
-
-                            customElements.define(n.tagName.toLowerCase(), elementClass as any);
-                        }
+                        await loading[tagName];
 
                         definedElements.push(n.tagName);
                     }
@@ -50,6 +66,7 @@ const observer = new MutationObserver(async (mutations) => {
 
     for (const e of definedElements) {
         delete elements[e];
+        delete loading[e];
     }
 
     if (Object.keys(elements).length === 0) {
