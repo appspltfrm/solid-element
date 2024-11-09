c04afdc8290d4defbe1e13b7604ccbb3c6f3663f
Mark Chris Levy <levy@codeandmore.pl>
2024-11-09 23:31:42 +0100

    commit

--- dist/CustomElementJSXEvents.d.ts
+++ dist/CustomElementJSXEvents.d.ts
@@ -7,11 +7,11 @@ export type CustomElementJSXEvents<Element extends CustomElement, Type extends {
 } & {
     [P in keyof Type as `on${Lowercase<string & P>}`]?: JSX.EventHandlerUnion<Element, Type[P]>;
 } & {
-    [P in keyof Type as `on:${string & P}`]?: JSX.EventHandlerWithOptionsUnion<Element, Type[P]>;
+    [P in keyof Type as `on:${string & P}`]?: JSX.EventHandlerWithOptions<Element, Type[P], JSX.EventHandler<Element, Type[P]>>;
 } & {
-    [P in keyof Type as `on:${Lowercase<string & P>}`]?: JSX.EventHandlerWithOptionsUnion<Element, Type[P], JSX.EventHandler<Element, Type[P]>>;
+    [P in keyof Type as `on:${Lowercase<string & P>}`]?: JSX.EventHandlerWithOptions<Element, Type[P], JSX.EventHandler<Element, Type[P]>>;
 } & {
-    [P in keyof Type as `oncapture:${string & P}`]?: JSX.EventHandlerWithOptionsUnion<Element, Type[P]>;
+    [P in keyof Type as `oncapture:${string & P}`]?: JSX.EventHandlerWithOptions<Element, Type[P]>;
 } & {
-    [P in keyof Type as `oncapture:${Lowercase<string & P>}`]?: JSX.EventHandlerWithOptionsUnion<Element, Type[P]>;
+    [P in keyof Type as `oncapture:${Lowercase<string & P>}`]?: JSX.EventHandlerWithOptions<Element, Type[P]>;
 };
