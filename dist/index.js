4ac575adcf1f2899dc33b40fdadc85cf0b0eb174
Mark Chris Levy <levy@codeandmore.pl>
2025-07-31 21:37:16 +0200

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
-import { createElementMemo, createElementSignal, createElementStore, createElementVar, deleteElementSignal, deleteElementStore, deleteElementVar, getElementMemo, getElementSignal, getElementStore, getElementVar, loadElementSignal, loadElementStore, setElementSignal, setElementStore, setElementVar, useElementMemo, useElementSignal, useElementStore } from "./vars.js";
 import { customElement } from "./customElement.js";
-import { defineCustomElement } from "./defineCustomElement.js";
-import { defineComponent } from "./defineComponent.js";
-import { isCustomElement } from "./isCustomElement.js";
-import { reactive } from "./reactive.js";
 import { customElementBirthmark } from "./customElementBirthmark.js";
+import { defineComponent } from "./defineComponent.js";
+import { defineCustomElement } from "./defineCustomElement.js";
 import { defineLazyCustomElement } from "./defineLazyCustomElement.js";
 import { getCustomElementOwner } from "./getCustomElementOwner.js";
+import { isCustomElement } from "./isCustomElement.js";
+import { reactive } from "./reactive.js";
+import { createElementMemo, createElementSignal, createElementStore, createElementVar, deleteElementSignal, deleteElementStore, deleteElementVar, getElementMemo, getElementSignal, getElementStore, getElementVar, loadElementSignal, loadElementStore, setElementSignal, setElementStore, setElementVar, useElementMemo, useElementSignal, useElementStore } from "./vars.js";
 export {
   createElementMemo,
   createElementSignal,
